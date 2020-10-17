dd9f638d68d09f91c047f7782146c5c75eca0c7a
Cesar Cordoba <cordobacesar29@gmail.com>
2020-10-17 16:51:21 -0300

    creando una API con JSON Server

---
+++ js/editarcliente.js
@@ -0,0 +1,55 @@
+import { mostrarAlerta, validar } from './funciones.js';
+import {obtenerCliente, editarCliente } from './API.js';
+
+(function () {
+
+    // campos del formulario
+    const nombreInput = document.querySelector('#nombre');
+    const emailInput = document.querySelector('#email');
+    const empresaInput = document.querySelector('#empresa');
+    const telefonoInput = document.querySelector('#telefono');
+    const idInput = document.querySelector('#id');
+
+    document.addEventListener('DOMContentLoaded',async () => {
+        const parametrosURL = new URLSearchParams(window.location.search);
+        const idCliente = parseInt( parametrosURL.get('id') );
+
+        const cliente = await obtenerCliente(idCliente);
+
+        mostrarCliente(cliente);
+
+        // submit de formulario guardar cambios
+        const formulario = document.querySelector('#formulario');
+        formulario.addEventListener('submit', validarCliente);
+    });
+
+    function mostrarCliente(cliente) {
+        const { nombre, empresa, email, telefono, id} = cliente;
+        nombreInput.value = nombre;
+        empresaInput.value = empresa;
+        emailInput.value = email;
+        telefonoInput.value = telefono;
+        idInput.value = id;
+    }
+
+    function validarCliente(e) {
+        e.preventDefault();
+
+        const cliente = {
+            nombre: nombreInput.value,
+            email: emailInput.value,
+            telefono: telefonoInput.value,
+            empresa: empresaInput.value,
+            id: parseInt(idInput.value)
+        }
+
+        if( validar(cliente)) {
+            mostrarAlerta('Todos los campos son obligatorios');
+            return;
+        }
+       
+        // reescribe el objeto
+        editarCliente(cliente);
+
+    }
+})();
